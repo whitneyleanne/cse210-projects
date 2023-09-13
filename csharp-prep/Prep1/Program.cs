f4dc34824b2ba9eb05b9ecded9e5253e02fc5f30
whitleanne <han17087@byui.edu>
2023-09-13 09:54:44 -0600

    Update to readme file

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -1,9 +1,24 @@
 using System;
 
-class Program
+Console.WriteLine("Hello Prep1 World!");
+
+//string interpolation
+int myInt = 5;
+Console.WriteLine("my int = " + {myInt});
+Console.WriteLine($"my int = {myInt}")
+
+//read line
+Console.Write("What is your name? ");
+string name = Console.ReadLine();
+Console.WriteLine($"name = {name}");
+
+
+//conditionals
+if (name == "Whitney")
 {
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello Prep1 World!");
-    }
+    Console.WriteLine("Hey that's me!");
+}
+else
+{
+    Console.WriteLine($"Hi there {name}");
 }
