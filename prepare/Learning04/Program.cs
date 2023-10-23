3fbea3fc5bfa7a644a7c841b9749d1fa6347281d
whitleanne <han17087@byui.edu>
2023-10-23 17:46:02 -0600

    Project 3 finishing updates, comments were added.

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -1,23 +1,110 @@
 using System;
+using System.Data.Common;
+using System.Security.Cryptography.X509Certificates;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Fraction f1 = new Fraction();
-        Console.WriteLine(f1.GetFractionString());
-        Console.WriteLine(f1.GetDecimalValue());
-
-        Fraction f2 = new Fraction(5);
-        Console.WriteLine(f2.GetFractionString());
-        Console.WriteLine(f2.GetDecimalValue());
-
-        Fraction f3 = new Fraction(3, 4);
-        Console.WriteLine(f3.GetFractionString());
-        Console.WriteLine(f3.GetDecimalValue());
-
-        Fraction f4 = new Fraction(1, 3);
-        Console.WriteLine(f4.GetFractionString());
-        Console.WriteLine(f4.GetDecimalValue());
     }
 }
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         var person = new Person("id1", "Person1");
+//         var student = new Student("id2", "Student 2", "Software Engineering");
+//         var employee = new Employee("id3", "Employee3", "CSEE");
+
+//         List<Person> people = new List<Person>();
+//         people.Add(person);
+//         people.Add(student);
+//         people.Add(employee);
+//     }
+// }
+
+// // 10/23/23 Class Notes on a class and subclasses
+// public class Person {
+//     private string _id;
+//     protected string _name; //this makes it available to use in other parts of code
+
+//     public Person(string id, string name) {
+//         _id = id;
+//         _name = name;
+//     }
+
+//     public void Display() {
+//         Console.WriteLine($"Name = {_name}");
+
+//     }
+// }
+
+// // student 
+// public class Student: Person {
+//     private string _major;
+
+//     public Student(string studentId, string studentName, string major) : base(studentId, studentName) {
+//         _major = major;
+//     }
+
+//     public void DisplayStudentSummary() {
+//         System.Console.WriteLine($"{_name}: {_major}");
+//     }
+// }
+
+// //employee
+// public class Employee: Person {
+//     private string _department;
+
+//     public Employee(string employeeId, string employeeName, string department) : base (employeeId, employeeName) {
+//         _department = department;
+//     }
+// }
+
+
+//         var vehicle = new Vehicle("id1", "Vehicle1");
+//         var car = new Car("id2", "Car2", "Honda");
+//         var truck = new Truck("id3", "Truck3", "Ford");
+
+//         List<Vehicle> people = new List<Person>();
+//         people.Add(vehicle);
+//         people.Add(car);
+//         people.Add(truck);
+//     }
+// }
+
+// // 10/23/23 Class Notes on a class and subclasses
+// public class Vehicle {
+//     private string _make;
+//     private string _model; 
+//     private int _miles;
+
+//     public Vehicle(string make, string model, int miles) {
+//         _make = make;
+//         _model = model;
+//         _miles = miles;
+//     }
+
+
+
+// }
+
+// // car 
+// public class Car: Vehicle {
+//     private string _major;
+
+//     public Car(string make, string model, int miles) : base(make, model, miles) {
+    
+//     }
+
+// }
+
+// //truck
+// public class Truck: Vehicle {
+//     private string _towing;
+
+//     public Truck(string make, string model, int miles, string towing) : base (make, model, miles, towing) {
+//         _towing = towing;
+//     }
+// }
--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,10 +1,64 @@
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
 
+// This class manages the random class throughout the application
+// It manages consistency!
+public static class Utils
+{
+    public static readonly Random Random = new Random();
+}
+
+//This class extends the functionality of generic lists and provides a method to shuffle the elements within the list in a random order.
+//ChatGPT and my brother helped me figure this out.
+public static class Extensions
+{
+    public static void Shuffle<T>(this IList<T> list)
+    {
+        int n = list.Count;
+        while (n > 1)
+        {
+            n--;
+            int k = Utils.Random.Next(n + 1);
+            T value = list[k];
+            list[k] = list[n];
+            list[n] = value;
+        }
+    }
+}
+
+//this is a manager for the scripture objects, allowing scriptures to be added to an internal list and then be selected at random from the collection
+public class ScriptureLibrary
+{
+    private List<Scripture> scriptures;
+
+    public ScriptureLibrary()
+    {
+        scriptures = new List<Scripture>();
+    }
+
+    public void AddScripture(Reference reference, string text)
+    {
+        scriptures.Add(new Scripture(reference, text));
+    }
+
+    public Scripture GetRandomScripture()
+    {
+        if (!scriptures.Any())
+        {
+            Console.WriteLine("The library is empty.");
+            return null;
+        }
+        int index = Utils.Random.Next(scriptures.Count);
+        return scriptures[index];
+    }
+}
+
+//This class represents the individual words in a scripture
 public class Word
 {
-    public string Text { get; set; }
+    public string Text { get; private set; }
     public bool IsHidden { get; set; }
 
     public Word(string text)
@@ -19,90 +73,128 @@ public class Word
     }
 }
 
+//Represents a complete scripture and holds a reference
 public class Scripture
 {
+    public Reference Reference { get; }
     private List<Word> words;
-    private int currentIndex;
-    private Random random;
 
-    public Scripture(string text)
+    public Scripture(Reference reference, string text)
     {
+        Reference = reference;
         words = text.Split(' ').Select(word => new Word(word)).ToList();
-        currentIndex = 0;
-        random = new Random();
     }
 
-    public bool HasWordsToDisplay()
+    public string GetScriptureText()
     {
-        return currentIndex < words.Count;
-    }
-
-    public List<Word> GetNextWords(int count)
-    {
-        var wordsToDisplay = new List<Word>();
-        for (int i = 0; i < count && currentIndex < words.Count; i++)
-        {
-            wordsToDisplay.Add(words[currentIndex]);
-            currentIndex++;
-        }
-        return wordsToDisplay;
+        return string.Join(" ", words);
     }
 
     public void HideRandomWords(int count)
     {
-        var hiddenWords = new List<Word>();
-        for (int i = 0; i < count; i++)
-        {
-            if (currentIndex >= words.Count)
-                break;
+        var wordsToHide = words.Where(w => !w.IsHidden).ToList();
 
-            int randomIndex = random.Next(currentIndex, words.Count);
-            if (!words[randomIndex].IsHidden)
-            {
-                words[randomIndex].IsHidden = true;
-                hiddenWords.Add(words[randomIndex]);
-            }
+        if (!wordsToHide.Any())
+        {
+            Console.WriteLine("No more words to hide! Press any key to continue.");
+            Console.ReadKey();
+            return;
         }
 
-        Console.Clear();
-        Console.WriteLine(string.Join(" ", words.Select(word => word.ToString())));
-        Console.WriteLine("Press Enter to continue or type 'quit' to end the program.");
-        Console.WriteLine("Hidden Words: " + string.Join(", ", hiddenWords.Select(word => word.Text)));
+        wordsToHide.Shuffle();
+        for (int i = 0; i < Math.Min(count, wordsToHide.Count); i++)
+        {
+            wordsToHide[i].IsHidden = true;
+        }
+    }
+
+    public bool WordsLeftToHide()
+    {
+        return words.Any(w => !w.IsHidden);
     }
 }
 
+//This holds the detailed reference information and makes sure the data is grouped logically.
 public class Reference
 {
-    public static void DisplayScripture(Scripture scripture)
-    {
-        while (scripture.HasWordsToDisplay())
-        {
-            Console.Clear();
-            Console.WriteLine(string.Join(" ", scripture.GetNextWords(3)));
-            Console.WriteLine("Press Enter to continue or type 'quit' to end the program: ");
-            string input = Console.ReadLine();
-            if (input == "quit")
-                break;
+    public string Book { get; }
+    public int Chapter { get; }
+    public int Verse { get; }
 
-            scripture.HideRandomWords(3);
+    public Reference(string book, int chapter, int verse)
+    {
+        Book = book;
+        Chapter = chapter;
+        Verse = verse;
+    }
+
+    public override string ToString()
+    {
+        return $"{Book} {Chapter}:{Verse}";
+    }
+}
+
+//This begins the scripture memorization session, allowing the user to interact to hide words.
+public class ScriptureMemorization
+{
+    public static void StartMemorizationSession(Scripture scripture)
+    {
+        Console.Clear();
+        Console.WriteLine(scripture.Reference);
+        Console.WriteLine(scripture.GetScriptureText());
+
+        while (scripture.WordsLeftToHide())
+        {
+            Console.WriteLine("\nPress Enter to hide words or type 'quit' to stop.");
+            string input = Console.ReadLine()?.Trim().ToLower();
+
+            if (string.IsNullOrEmpty(input))
+            {
+                scripture.HideRandomWords(3); // Hiding three words at a time
+                Console.Clear();
+                Console.WriteLine(scripture.Reference);
+                Console.WriteLine(scripture.GetScriptureText());
+            }
+            else if (input == "quit")
+            {
+                Console.WriteLine("\nEnd of session.");  // Moved here so the user sees it before the session ends
+                break;
+            }
         }
     }
 }
 
+//This class initializes the data, handles the main loop of user interactions, and triggers memorization commands based on the user interactions.
 class Program
 {
     static void Main(string[] args)
     {
-        string scriptureText = "Your scripture text goes here."; // Replace with your scripture text.
-        Scripture scripture = new Scripture(scriptureText);
+        ScriptureLibrary library = new ScriptureLibrary();
 
-        Console.WriteLine("Welcome to the Scripture Display Program!");
-        Console.WriteLine("Press Enter to begin displaying the scripture.");
-        Console.ReadLine();
+        // Adding scriptures to the library
+            library.AddScripture(new Reference("Nephi", 2, 27), "Wherefore, men are free according to the flesh; and all things are given them which are expedient unto man. And they are free to choose liberty and eternal life, through the great Mediator of all men, or to choose captivity and death, according to the captivity and power of the devil; for he seeketh that all men might be miserable like unto himself.");
+            library.AddScripture(new Reference("Nephi", 2, 16), "Wherefore, the Lord God gave unto man that he should act for himself. Wherefore, man could not act for himself save it should be that he was enticed by the one or the other.");
+            library.AddScripture(new Reference("Moroni", 10, 5), "And by the power of the Holy Ghost ye may know the truth of all things.");
 
-        Reference.DisplayScripture(scripture);
+        Console.WriteLine("Welcome to the Scripture Memorization Helper!");
 
-        Console.WriteLine("Program ended. Press any key to exit.");
-        Console.ReadKey();
+        while (true)
+        {
+            Console.WriteLine("Press Enter to display a new scripture or type 'quit' to exit.");
+            string command = Console.ReadLine();
+
+            if (command.Equals("quit", StringComparison.OrdinalIgnoreCase))
+            {
+                break;
+            }
+
+            var scripture = library.GetRandomScripture();
+            if (scripture != null)
+            {
+                ScriptureMemorization.StartMemorizationSession(scripture);
+            }
+        }
+
+        Console.WriteLine("Thank you for using the Scripture Memorization Helper. Goodbye!");
     }
 }
