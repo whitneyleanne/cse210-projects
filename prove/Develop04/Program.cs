b3699d9dcf4ec057dc57b06d8f1c9c55a95961a7
whitleanne <han17087@byui.edu>
2023-11-04 22:07:08 -0600

    Project 4 Completed.

--- prove/Develop04/Program.cs
+++ prove/Develop04/Program.cs
@@ -2,14 +2,223 @@ using System;
 
 class Program
 {
-    static void Main(string[] args)
+public static void Main()
+{
+    while (true)
     {
-        Console.WriteLine("Hello Develop04 World!");
+        Console.WriteLine("Choose an activity: \n1. Breathing \n2. Reflection \n3. Listing \n4. Exit");
+        int choice = Convert.ToInt32(Console.ReadLine());
+
+        ActivityBase activity = null;
+
+        switch (choice)
+        {
+            case 1:
+                activity = new BreathingActivity();
+                break;
+            case 2:
+                activity = new ReflectionActivity();
+                break;
+            case 3:
+                activity = new ListingActivity();
+                break;
+            case 4:
+                return;
+            default:
+                Console.WriteLine("Invalid choice. Try again.");
+                continue;
+        }
+
+        activity.StartActivity();
+        activity.Execute();
+        activity.EndActivity();
     }
+}
+
+}
+
+public abstract class ActivityBase
+{
+    protected string name;
+    protected string description;
+    protected int duration;
+
+    public ActivityBase(string name, string description)
+    {
+        this.name = name;
+        this.description = description;
+    }
+
+    public void StartActivity()
+    {
+        Console.WriteLine($"Welcome to the {name} activity.");
+        Console.WriteLine(description);
+        Console.Write("Please set the duration of the activity in seconds: ");
+        duration = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Prepare to begin...");
+        ShowAnimation(3);  // Assuming a 3-second pause before starting
+    }
+
+    public void EndActivity()
+    {
+        Console.WriteLine("\nYou have done a good job!");
+        ShowAnimation(3);
+        Console.WriteLine($"You have completed the {name} activity for {duration} seconds.");
+        ShowAnimation(3);
+    }
+
+    protected void ShowAnimation(int seconds)
+    {
+        char[] animationChars = new char[] { '|', '/', '-', '\\' };
+        for (int i = 0; i < seconds; i++)
+    {
+            foreach (var animChar in animationChars)
+        {
+                Console.Write("\r" + animChar);  // "\r" is used to overwrite the current line in the console.
+                Thread.Sleep(250);  // Wait for a quarter second before showing the next character
+        }
+    }
+        Console.WriteLine();  // Move to the next line after the animation is done
+    }
+
+    public abstract void Execute();  // This method will be implemented by each derived class
 }
 
 // THREE activities
 // first one: breathe in and out, 5-6 seconds each, six times? 
+public class BreathingActivity : ActivityBase
+{
+    public BreathingActivity() : base("Breathing", "This activity will help you relax by walking you through breathing in and out slowly. Clear your mind and focus on your breathing.")
+    {
+    }
+
+    public override void Execute()
+    {
+        int cycles = duration / 6;  // Assuming 3 seconds for "Breathe in" and 3 seconds for "Breathe out"
+        for (int i = 0; i < cycles; i++)
+        {
+            Console.WriteLine("Breathe in...");
+            ShowAnimation(3);
+            Console.WriteLine("Breathe out...");
+            ShowAnimation(3);
+        }
+    }
+}
+
 // second one: pulls from menu, activity for reflection where prompts 
+
+public class ReflectionActivity : ActivityBase
+{
+    private List<string> reflectionPrompts;
+    private List<string> reflectionQuestions;
+    private Random random;
+
+    public ReflectionActivity() : base("Reflection", "This activity will help you reflect on times in your life when you have shown strength and resilience. This will help you recognize the power you have and how you can use it in other aspects of your life.")
+    {
+        reflectionPrompts = new List<string>
+        {
+            "Think of a time when you stood up for someone else.",
+            "Think of a time when you did something really difficult.",
+            "Think of a time when you helped someone in need.",
+            "Think of a time when you did something truly selfless."
+        };
+
+        reflectionQuestions = new List<string>
+        {
+            "Why was this experience meaningful to you?",
+            "Have you ever done anything like this before?",
+            "How did you get started?",
+            "How did you feel when it was complete?",
+            "What made this time different than other times when you were not as successful?",
+            "What is your favorite thing about this experience?",
+            "What could you learn from this experience that applies to other situations?",
+            "What did you learn about yourself through this experience?",
+            "How can you keep this experience in mind in the future?"
+        };
+
+        random = new Random();
+    }
+
+    public override void Execute()
+    {
+        var prompt = GetRandomPrompt();
+        Console.WriteLine(prompt);
+
+        int questionTime = duration / 3;  // Assuming we ask 3 questions in the given duration
+        for (int i = 0; i < 3; i++)
+        {
+            var question = GetRandomQuestion();
+            Console.WriteLine(question);
+            ShowAnimation(questionTime);
+        }
+    }
+
+    private string GetRandomPrompt()
+    {
+        int index = random.Next(reflectionPrompts.Count);
+        var prompt = reflectionPrompts[index];
+        reflectionPrompts.RemoveAt(index);  // Remove used prompt to avoid repetition
+        return prompt;
+    }
+
+    private string GetRandomQuestion()
+    {
+        int index = random.Next(reflectionQuestions.Count);
+        var question = reflectionQuestions[index];
+        reflectionQuestions.RemoveAt(index);  // Remove used question to avoid repetition
+        return question;
+    }
+}
+
 // third: listing activity - list as many responses as you can to the following prompt
+
+public class ListingActivity : ActivityBase
+{
+    private List<string> listingPrompts;
+    private Random random;
+
+    public ListingActivity() : base("Listing", "This activity will help you reflect on the good things in your life by having you list as many things as you can in a certain area.")
+    {
+        listingPrompts = new List<string>
+        {
+            "Who are people that you appreciate?",
+            "What are personal strengths of yours?",
+            "Who are people that you have helped this week?",
+            "When have you felt the Holy Ghost this month?",
+            "Who are some of your personal heroes?"
+        };
+
+        random = new Random();
+    }
+
+    public override void Execute()
+    {
+        var prompt = GetRandomPrompt();
+        Console.WriteLine(prompt);
+
+        Console.WriteLine("Press enter after listing each item.");
+        int count = 0;
+
+        DateTime endTime = DateTime.Now.AddSeconds(duration);
+        while (DateTime.Now <= endTime)
+        {
+            string listItem = Console.ReadLine();
+            if (!string.IsNullOrWhiteSpace(listItem))
+            {
+                count++;
+            }
+        }
+
+        Console.WriteLine($"You listed {count} items.");
+    }
+
+    private string GetRandomPrompt()
+    {
+        int index = random.Next(listingPrompts.Count);
+        var prompt = listingPrompts[index];
+        listingPrompts.RemoveAt(index);  // Remove used prompt to avoid repetition
+        return prompt;
+    }
+}
+
 // once any of the three activities are finished, bring user back to menu
